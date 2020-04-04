b646e9441ed3cf9ea3fbda3511b976aa02413426
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 23:58:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,47 +1,47 @@
 # require 'pry'
 
-class Transfer
-  # code here
-  attr_accessor :sender, :receiver, :amount, :status, :count
+# class Transfer
+#   # code here
+#   attr_accessor :sender, :receiver, :amount, :status, :count
 
-  def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
-  end
+#   def initialize(sender, receiver, amount)
+#     @sender = sender
+#     @receiver = receiver
+#     @amount = amount
+#     @status = "pending"
+#   end
 
-  def both_valid?
-    if sender.valid? && receiver.valid?
-      true
-    else
-      false
-    end
-  end
+#   def both_valid?
+#     if sender.valid? && receiver.valid?
+#       true
+#     else
+#       false
+#     end
+#   end
 
-  def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+#   def execute_transaction
+#     if @sender.balance < @amount
+#       @status = "rejected"
+#       return "Transaction rejected. Please check your account balance."
 
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
-    end
-  end
+#     elsif @status == "complete"
+#       puts "Transaction was already excuted"
+#     else
+#       @sender.deposit( @amount * -1 ) 
+#       @receiver.deposit( @amount )
+#       @status = "complete"
+#     end
+#   end
 
-  def reverse_transfer
-    if @status == "complete"
-      @sender.deposit( @amount ) 
-      @receiver.deposit( @amount * -1)
-      @status = "reversed"
-    end
-  end
+#   def reverse_transfer
+#     if @status == "complete"
+#       @sender.deposit( @amount ) 
+#       @receiver.deposit( @amount * -1)
+#       @status = "reversed"
+#     end
+#   end
 
-end
+# end
 # class Transfer
 #   attr_accessor :sender, :receiver, :amount, :status
 
