87de98b80090e2a79b9a9ea09c40f5f8d8b7ccf8
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 23:50:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       @status = "complete"
     else
       # @status = "rejected"
-       return @status = "Transaction rejected. Please check your account balance."
+       return @status == "Transaction rejected. Please check your account balance."
     end
   end
 
