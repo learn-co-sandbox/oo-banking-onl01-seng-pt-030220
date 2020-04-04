d33d05b284a8e35f9b091b5b6235092ca2cec64e
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 23:55:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,34 +1,34 @@
-class BankAccount
-  # code here
-  attr_accessor :balance, :status
-  attr_reader :name
+# class BankAccount
+#   # code here
+#   attr_accessor :balance, :status
+#   attr_reader :name
 
-  def initialize(name)
-    @name = name
-    @balance = 1000
-    @status = "open"
-  end
+#   def initialize(name)
+#     @name = name
+#     @balance = 1000
+#     @status = "open"
+#   end
 
-  def deposit(amount)
-    @balance += amount
-  end
+#   def deposit(amount)
+#     @balance += amount
+#   end
 
-  def display_balance
-    "Your Balance is $#{@balance}."
-  end
+#   def display_balance
+#     "Your Balance is $#{@balance}."
+#   end
 
-  def valid?
-    if @status == "open" && @balance > 0
-      true
-    else
-      false
-    end
-  end
+#   def valid?
+#     if @status == "open" && @balance > 0
+#       true
+#     else
+#       false
+#     end
+#   end
 
-  def close_account
-    @status = "closed"
-  end
-end
+#   def close_account
+#     @status = "closed"
+#   end
+# end
 
 # class BankAccount
 #   attr_accessor :balance, :status
